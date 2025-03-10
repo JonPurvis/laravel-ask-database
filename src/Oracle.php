30785ebdb5e74bc6a7b85605d33049d5ca7075dd
JonPurvis <jonathan.purvis@visualsoft.co.uk>
2025-03-10 03:32:34 +0000

    wip

--- src/Oracle.php
+++ src/Oracle.php
@@ -25,7 +25,7 @@ class Oracle
 
         $prompt = $this->buildPrompt($question, $query, $result);
 
-        $answer = $this->queryOpenAi($prompt, "\n\n", 0.7);
+        $answer = $this->queryOpenAi($prompt, "\n", 0.7);
 
         return Str::of($answer)
             ->trim()
@@ -53,7 +53,7 @@ class Oracle
             'prompt' => $prompt,
             'temperature' => $temperature,
             'max_tokens' => 1000,
-            'stop' => '...',
+            'stop' => $stop,
         ]);
 
         return $completions->choices[0]->text;
